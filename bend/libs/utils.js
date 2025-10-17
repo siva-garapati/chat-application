8cf511db3400e37b48a86700847c51d7002a489f
siva-garapati <sivagarapati999@gmail.com>
2025-10-18 01:45:11 +0530

    fix cookies bug

--- bend/libs/utils.js
+++ bend/libs/utils.js
@@ -5,7 +5,8 @@ const setCookies = (userId, res) =>{
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        // sameSite: "strict",
+        secure:true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
