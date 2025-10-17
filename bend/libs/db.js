4fa293175bf491e8e5225d786b02122765f29c3a
siva-garapati <sivagarapati999@gmail.com>
2025-10-17 23:16:05 +0530

    changed db

--- bend/libs/db.js
+++ bend/libs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI_PRODUCTION);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection error:", error);
